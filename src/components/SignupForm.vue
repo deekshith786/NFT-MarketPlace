6ff6942ac4764a395cbd6dd24eb1199c84c228bc
Deekshith <deekshith.m@comakeit.com>
2023-06-01 16:46:51 +0530

    wip

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,35 +1,39 @@
 <template>
     <div>
-        <v-container fluid class="pa-0">
-            <v-row class="pa-10 ma-10">
+        <v-container fluid>
+            <v-row class="pa-2 ma-2">
                 <v-col>
                     <v-img src="public/login.png"></v-img>
                 </v-col>
-                <v-col class="pl-15">
+                <v-col>
                     <div>
                         <h1>Create Account</h1>
                         <h2 class="font-weight-light my-2">Welcome! Enter Your Details And Start Creating, Collection And
                             Selling
                             NFTs. </h2>
                     </div>
-                    <form>
-                        <v-responsive max-width="350">
-                            <v-text-field bg-color="#ffffff" class="text-black" rounded="xl" v-model="userName.value.value"
-                                label="username"></v-text-field>
+                    <form @submit.prevent="submit">
+                        <v-responsive max-width="500">
+                            <v-text-field variant="outline" prepend-inner-icon="mdi mdi-account-outline" bg-color="#ffffff"
+                                class="text-black" rounded="xl" v-model="userName.value.value"
+                                placeholder="username"></v-text-field>
                         </v-responsive>
-                        <v-responsive max-width="350">
-                            <v-text-field bg-color="#ffffff" class="text-black" rounded="xl" v-model="email.value.value"
-                                label="email"></v-text-field>
+                        <v-responsive max-width="500">
+                            <v-text-field variant="outline" prepend-inner-icon="mdi mdi-email-outline" bg-color="#ffffff"
+                                class="text-black" rounded="xl" v-model="email.value.value"
+                                placeholder="email"></v-text-field>
                         </v-responsive>
-                        <v-responsive max-width="350">
-                            <v-text-field bg-color="#ffffff" class="text-black" rounded="xl" v-model="password.value.value"
-                                label="password"></v-text-field>
+                        <v-responsive max-width="500">
+                            <v-text-field variant="outline" prepend-inner-icon="mdi mdi-lock-outline" bg-color="#ffffff"
+                                class="text-black" rounded="xl" v-model="password.value.value"
+                                placeholder="password"></v-text-field>
                         </v-responsive>
-                        <v-responsive max-width="350">
-                            <v-text-field bg-color="#ffffff" class="text-black" rounded="xl"
-                                v-model="confirmPassword.value.value" label="Confirm Password"></v-text-field>
+                        <v-responsive max-width="500">
+                            <v-text-field variant="outline" prepend-inner-icon="mdi mdi-lock-outline" bg-color="#ffffff"
+                                class="text-black" rounded="xl" v-model="confirmPassword.value.value"
+                                placeholder="Confirm Password"></v-text-field>
                         </v-responsive>
-                        <v-btn class="text-white" flat type="submit" color="customButton" width="350px" height="50px"
+                        <v-btn class="text-white" flat type="submit" color="customButton" width="500px" height="50px"
                             rounded>Create
                             Account</v-btn>
                     </form>
@@ -44,7 +48,6 @@ import { useField, useForm } from 'vee-validate'
 
 export default {
     name: "SignUp",
-
     setup() {
 
         const userName = useField('userName')
@@ -52,8 +55,43 @@ export default {
         const password = useField('password')
         const confirmPassword = useField('confirmPassword')
 
+
+
+        const { handleSubmit, handleReset } = useForm({
+            validationSchema: {
+                name(value) {
+                    if (value?.length >= 2) return true
+
+                    return 'Name needs to be at least 2 characters.'
+                },
+                phone(value) {
+                    if (value?.length > 9 && /[0-9-]+/.test(value)) return true
+
+                    return 'Phone number needs to be at least 9 digits.'
+                },
+                email(value) {
+                    if (/^[a-z.-]+@[a-z.-]+\.[a-z]+$/i.test(value)) return true
+
+                    return 'Must be a valid e-mail.'
+                },
+                select(value) {
+                    if (value) return true
+
+                    return 'Select an item.'
+                },
+                checkbox(value) {
+                    if (value === '1') return true
+
+                    return 'Must be checked.'
+                },
+            },
+        })
+        const submit = handleSubmit(values => {
+            alert(JSON.stringify(values, null, 2))
+        })
+
         return {
-            userName, email, password, confirmPassword
+            userName, email, password, confirmPassword, submit, handleReset
         }
     }
 
