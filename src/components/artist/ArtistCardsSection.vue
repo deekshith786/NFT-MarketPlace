b0546855ed3a5b87ff454d3407b8b446451f5f74
Deekshith <deekshith.m@comakeit.com>
2023-06-09 11:48:03 +0530

    image paths

--- src/components/artist/ArtistCardsSection.vue
+++ src/components/artist/ArtistCardsSection.vue
@@ -55,15 +55,15 @@ export default {
         const tab = ref(null)
         const artists = ref(
             [
-                { name: 'AstroFiction', role: "Total Sales: 34.53 ETH", avatar: '/public/Avatar.png', image: "/public/ArtistThree.png" },
-                { name: 'Space Tales', role: "Total Sales: 34.53 ETH", avatar: '/public/Avatar.png', image: "/public/ArtistSix.png" },
-                { name: 'CryptoCity', role: "Total Sales: 34.53 ETH", avatar: '/public/Avatar.png', image: "/public/ArtistFour.png" },
-                { name: 'ColorfulDog 0524', role: "Total Sales: 34.53 ETH", avatar: '/public/Avatar.png', image: "/public/ArtistFive.png" },
-                { name: 'Cherry Blossom Girl 037', role: "Total Sales: 34.53 ETH", avatar: '/public/Avatar.png', image: "/public/ArtistSeven.png" },
-                { name: 'IceCream Ape', role: "Total Sales: 34.53 ETH", avatar: '/public/Avatar.png', image: "/public/ArtistNine.png" },
-                { name: 'Life On Edena', role: "Total Sales: 34.53 ETH", avatar: '/public/Avatar.png', image: "/public/ArtistTwo.png" },
-                { name: 'Dancing Robots 0987', role: "Total Sales: 34.53 ETH", avatar: '/public/Avatar.png', image: "/public/ArtistEight.png" },
-                { name: 'Distant Galaxy', role: "Total Sales: 34.53 ETH", avatar: '/public/Avatar.png', image: "/public/ArtistOne.png" },
+                { name: 'CryptoCity', role: "Total Sales: 34.53 ETH", avatar: '/public/Avatar.png', image: "/ArtistFour.png" },
+                { name: 'AstroFiction', role: "Total Sales: 34.53 ETH", avatar: '/public/Avatar.png', image: "/ArtistThree.png" },
+                { name: 'Space Tales', role: "Total Sales: 34.53 ETH", avatar: '/public/Avatar.png', image: "/ArtistSix.png" },
+                { name: 'ColorfulDog 0524', role: "Total Sales: 34.53 ETH", avatar: '/public/Avatar.png', image: "/ArtistFive.png" },
+                { name: 'Cherry Blossom Girl 037', role: "Total Sales: 34.53 ETH", avatar: '/public/Avatar.png', image: "/ArtistSeven.png" },
+                { name: 'IceCream Ape', role: "Total Sales: 34.53 ETH", avatar: '/public/Avatar.png', image: "/ArtistNine.png" },
+                { name: 'Life On Edena', role: "Total Sales: 34.53 ETH", avatar: '/public/Avatar.png', image: "/ArtistTwo.png" },
+                { name: 'Dancing Robots 0987', role: "Total Sales: 34.53 ETH", avatar: '/public/Avatar.png', image: "/ArtistEight.png" },
+                { name: 'Distant Galaxy', role: "Total Sales: 34.53 ETH", avatar: '/public/Avatar.png', image: "/ArtistOne.png" },
             ])
         return {
             artists,
