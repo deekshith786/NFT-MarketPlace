66a9d16cb4ea390950fae02bd888b0eb17fbfa0a
Deekshith <deekshith.m@comakeit.com>
2023-06-19 15:45:15 +0530

    implementation of keycloak

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,37 +7,28 @@
         <v-btn variant="plain" class="mr-5 text-white" router to="/">Home</v-btn>
         <v-btn variant="plain" class="mr-5 text-white" router to="/market-place">Marketplace</v-btn>
         <v-btn variant="plain" class="mr-5 text-white" router to="/ranking">Rankings</v-btn>
-        <v-btn variant="plain" class="mr-5 text-white" router to="/connect-wallet">Connect a wallet</v-btn>
-        <!-- <v-btn prepend-icon="mdi mdi-account-outline" height="55px" width="150px" rounded variant="flat"
-            color="customButton" class="text-white" router to="/signup" v-if="$route.path === '/signup'">Sign
-            Up</v-btn> -->
-        <!-- <v-btn prepend-icon="mdi mdi-account-outline" height="55px" width="150px" rounded variant="flat"
-            color="customButton" class="text-white" router to="/signup">Sign
-            Up</v-btn> -->
+        <v-btn variant="plain" class="mr-5 text-white" v-if="!isAuthenticated" router to="/connect-wallet">Connect a wallet</v-btn>
         <v-btn prepend-icon="mdi mdi-account-outline" height="55px" width="150px" rounded variant="flat"
             color="customButton" class="text-white" @click="login" v-if="!isAuthenticated">Sign
             Up</v-btn>
-        <v-btn prepend-icon="mdi mdi-account-outline" height="55px" width="150px" rounded variant="flat"
-            color="customButton" class="text-white" @click="logout" v-else>logout
-        </v-btn>
-
-        <!-- <v-btn prepend-icon="mdi mdi-account-outline" height="55px" width="150px" rounded variant="flat"
-            color="customButton" class="text-white" @click="login" router to="/artist">
-            {{ isAuthenticated ? 'Profile' : 'Sign Up' }}
-        </v-btn> -->
-
-
-        <!-- <p v-else></p> -->
+        <v-menu min-width="200px" rounded theme="dark" v-else>
+            <template v-slot:activator="{ props }">
+                <v-btn icon v-bind="props">
+                    <v-avatar color="customButton" size="large">
+                        <v-icon icon="mdi mdi-account-outline"></v-icon>
+                    </v-avatar>
+                </v-btn>
+            </template>
+            <v-card fluid>
+                <v-card-text >
+                    <v-btn variant="text" router to = '/artist'> {{ $keycloak.tokenParsed.preferred_username }} </v-btn>
+                    <v-divider class="my-3"></v-divider>
+                    <v-btn variant="text" @click="logout"> Logout </v-btn>
+                </v-card-text>
+            </v-card>
+        </v-menu>
     </v-toolbar>
 </template>
-<!-- <script>
-
-
-export default {
-    name: "Navbar",
-    
-};
-</script> -->
 <script>
 import { ref, onBeforeMount } from 'vue';
 import keycloak from '../../src/keycloak.js'
@@ -46,8 +37,6 @@ import router from '@/router';
 
 export default {
     name: "Navbar",
-
-
     setup() {
         const isAuthenticated = ref(false);
 
@@ -63,26 +52,27 @@ export default {
         const login = () => {
             if (!isAuthenticated.value) {
                 keycloak.login();
+                console.log('Keycloak Initialization Successful');
+                console.log('Authenticated:', authenticated);
             } else {
                 console.log('User is already authenticated');
             }
         };
 
         const logout = () => {
-            keycloak.logout();
             isAuthenticated.value = false;
+            router.push('/');
+            keycloak.logout({ redirectUri: window.location.origin });
         };
 
         keycloak.onAuthSuccess = () => {
             isAuthenticated.value = true;
-            router.push('/artist');
+            router.push('/market-place');
         };
         keycloak.onAuthLogout = () => {
             isAuthenticated.value = false;
             router.push('/');
         };
-
-
         return {
             isAuthenticated,
             login,
